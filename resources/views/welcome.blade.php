2594d80e81db287bd466774c8c2652e65758ea01
leoOSX <agusosx@gmail.com>
2019-07-09 21:38:22 -0400

    Update welcome.blade.php
    cambio de nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    bytemo
                 </div>
 
                 <div class="links">
